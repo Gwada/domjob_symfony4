1b7ce480d0d4b015873c39146a72f96ecc1e4dd0
dlavaury <dlavaury@student.42.fr>
2018-12-12 15:15:31 +0100

    add api bundle

--- src/Entity/GrandDomaine.php
+++ src/Entity/GrandDomaine.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\GrandDomaineRepository")
  */
 class GrandDomaine
@@ -28,20 +30,8 @@ class GrandDomaine
      */
     private $libelleGrandDomaine;
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\DomaineProfessionnel", mappedBy="grandDomaine")
-     */
-    private $domainesProfessionnel;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Advert", mappedBy="grandDomaine")
-     */
-    private $adverts;
-
     public function __construct()
     {
-        $this->domainesProfessionnel = new ArrayCollection();
-        $this->adverts = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -69,68 +59,6 @@ class GrandDomaine
     public function setLibelleGrandDomaine(string $libelleGrandDomaine): self
     {
         $this->libelleGrandDomaine = $libelleGrandDomaine;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|DomaineProfessionnel[]
-     */
-    public function getDomainesProfessionnel(): Collection
-    {
-        return $this->domainesProfessionnel;
-    }
-
-    public function addDomainesProfessionnel(DomaineProfessionnel $domainesProfessionnel): self
-    {
-        if (!$this->domainesProfessionnel->contains($domainesProfessionnel)) {
-            $this->domainesProfessionnel[] = $domainesProfessionnel;
-            $domainesProfessionnel->setGrandDomaine($this);
-        }
-
-        return $this;
-    }
-
-    public function removeDomainesProfessionnel(DomaineProfessionnel $domainesProfessionnel): self
-    {
-        if ($this->domainesProfessionnel->contains($domainesProfessionnel)) {
-            $this->domainesProfessionnel->removeElement($domainesProfessionnel);
-            // set the owning side to null (unless already changed)
-            if ($domainesProfessionnel->getGrandDomaine() === $this) {
-                $domainesProfessionnel->setGrandDomaine(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Advert[]
-     */
-    public function getAdverts(): Collection
-    {
-        return $this->adverts;
-    }
-
-    public function addAdvert(Advert $advert): self
-    {
-        if (!$this->adverts->contains($advert)) {
-            $this->adverts[] = $advert;
-            $advert->setGrandDomaine($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAdvert(Advert $advert): self
-    {
-        if ($this->adverts->contains($advert)) {
-            $this->adverts->removeElement($advert);
-            // set the owning side to null (unless already changed)
-            if ($advert->getGrandDomaine() === $this) {
-                $advert->setGrandDomaine(null);
-            }
-        }
 
         return $this;
     }
